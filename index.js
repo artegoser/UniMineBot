29ac418b3057f99ef1953614bdd1f2d59a18668c
Artemy <artegoser@gmail.com>
2023-09-23 15:37:10 +0300

    feat: come to owner

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const mineflayer = require("mineflayer");
 const { mineflayer: mineflayerViewer } = require("prismarine-viewer");
+const {
+  pathfinder,
+  Movements,
+  goals: { GoalNear },
+} = require("mineflayer-pathfinder");
 require("dotenv").config();
 
 const bot = mineflayer.createBot({
@@ -9,14 +14,36 @@ const bot = mineflayer.createBot({
   port: Number(process.env.PORT),
 });
 
+bot.loadPlugin(pathfinder);
+
 bot.on("chat", (username, message) => {
   if (username === bot.username) return;
   if (username !== process.env.OWNER) return;
 
-  if (message.toLowerCase().startsWith(process.env.USER_NAME.toLowerCase()))
-    return bot.chat("Привет, хозяин!");
+  if (message.toLowerCase().startsWith(process.env.USER_NAME.toLowerCase())) {
+    if (username !== process.env.OWNER)
+      return bot.chat("Я слушаю только " + process.env.OWNER);
 
-  bot.chat("Ничего не понял, хозяин...");
+    let command_message = message
+      .replace(process.env.USER_NAME, "")
+      .trim()
+      .split(" ");
+
+    if (command_message[0] === "сюда") {
+      const target = bot.players[username]?.entity;
+      if (!target) {
+        bot.chat("Не могу найти тебя.");
+        return;
+      }
+      const { x: playerX, y: playerY, z: playerZ } = target.position;
+
+      bot.pathfinder.setMovements(defaultMove);
+      bot.pathfinder.setGoal(
+        new GoalNear(playerX, playerY, playerZ, RANGE_GOAL)
+      );
+    }
+  }
+  return bot.chat("Дарова");
 });
 
 bot.once("spawn", () => {
