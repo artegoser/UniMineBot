91e33502952fb587229be9d0546bb5f0744cfe12
Artemy <artegoser@gmail.com>
2023-09-23 15:56:59 +0300

    fix: movements

--- index.js
+++ index.js
@@ -16,37 +16,39 @@ const bot = mineflayer.createBot({
 
 bot.loadPlugin(pathfinder);
 
-bot.on("chat", (username, message) => {
-  if (username === bot.username) return;
-  if (username !== process.env.OWNER) return;
-
-  if (message.toLowerCase().startsWith(process.env.USER_NAME.toLowerCase())) {
-    if (username !== process.env.OWNER)
-      return bot.chat("Я слушаю только " + process.env.OWNER);
-
-    let command_message = message
-      .replace(process.env.USER_NAME, "")
-      .trim()
-      .split(" ");
-
-    if (command_message[0] === "сюда") {
-      const target = bot.players[username]?.entity;
-      if (!target) {
-        bot.chat("Не могу найти тебя.");
-        return;
-      }
-      const { x: playerX, y: playerY, z: playerZ } = target.position;
-
-      bot.pathfinder.setMovements(defaultMove);
-      bot.pathfinder.setGoal(
-        new GoalNear(playerX, playerY, playerZ, RANGE_GOAL)
-      );
-    }
-  }
-  return bot.chat("Дарова");
-});
-
 bot.once("spawn", () => {
+  const defaultMove = new Movements(bot);
+  bot.pathfinder.setMovements(defaultMove);
+
+  bot.on("chat", (username, message) => {
+    if (username === bot.username) return;
+    if (username !== process.env.OWNER) return;
+
+    if (message.toLowerCase().startsWith(process.env.USER_NAME.toLowerCase())) {
+      if (username !== process.env.OWNER)
+        return bot.chat("Я слушаю только " + process.env.OWNER);
+
+      let command_message = message
+        .replace(process.env.USER_NAME, "")
+        .trim()
+        .split(" ");
+
+      if (command_message[0] === "сюда") {
+        const target = bot.players[username]?.entity;
+        if (!target) {
+          bot.chat("Не могу найти тебя.");
+          return;
+        }
+        const { x: playerX, y: playerY, z: playerZ } = target.position;
+
+        bot.pathfinder.setGoal(
+          new GoalNear(playerX, playerY, playerZ, RANGE_GOAL)
+        );
+      }
+    }
+    return bot.chat("Дарова");
+  });
+
   bot.chat(process.env.INITIAL_COMMAND);
 });
 
